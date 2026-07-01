16864c93804da018d9e10700f4428a1c1d45cf0d
heryiyu <a0970927705@gmail.com>
2026-07-01 18:34:09 +0800

    fix: bump service worker cache version to clear browser cache and force script reload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'church-bible-reading-v81';
-const DYNAMIC_CACHE_NAME = 'church-bible-dynamic-v81';
+const CACHE_NAME = 'church-bible-reading-v82';
+const DYNAMIC_CACHE_NAME = 'church-bible-dynamic-v82';
 
 // Static resources to precache
 const PRECACHE_ASSETS = [
