ff7e0e5a39c5580553d57e38e289225c866b9a9c
heryiyu <a0970927705@gmail.com>
2026-07-28 15:54:33 +0800

    test: add unit tests for UserBadgeCard component

--- components/ui/__tests__/UserBadgeCard.test.tsx
+++ components/ui/__tests__/UserBadgeCard.test.tsx
@@ -1,5 +1,4 @@
 import { describe, it, expect, vi, beforeEach } from "vitest"
-import React from "react"
 import { UserBadgeCard } from "../UserBadgeCard"
 
 describe("UserBadgeCard Component Tests", () => {
